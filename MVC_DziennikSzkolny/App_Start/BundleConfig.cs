7dafe719b15fb8a22f3d6f2d41c0d352e99caeef
misspaula23 <koakowskapaulina@gmail.com>
2016-11-10 14:45:52 +0100

    ehhh

--- MVC_DziennikSzkolny/App_Start/BundleConfig.cs
+++ MVC_DziennikSzkolny/App_Start/BundleConfig.cs
@@ -9,7 +9,7 @@ namespace MVC_DziennikSzkolny
         public static void RegisterBundles(BundleCollection bundles)
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
-                        "~/Scripts/jquery-{version}.js"));
+                           "~/Scripts/jquery-{version}.js"));
 
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.validate*"));
@@ -26,6 +26,15 @@ namespace MVC_DziennikSzkolny
             bundles.Add(new StyleBundle("~/Content/css").Include(
                       "~/Content/bootstrap.css",
                       "~/Content/site.css"));
+
+            bundles.Add(new ScriptBundle("~/bundles/datetimepicker").Include(
+                                  "~/Scripts/bootstrap-datetimepicker.js",
+                                   "~/Scripts/moment.js"));
+
+            bundles.Add(new StyleBundle("~/Content/datetimepicker").Include(
+                      "~/Content/bootstrap-datetimepicker.css"));
+
+
         }
     }
 }
